d5554a4c7ea7542b98370484fc48cd2cbf5b94bc
Mister Magoo <mister.magoo+githubsql@gmail.com>
2019-04-22 23:02:26 +0100

    WIP - Needs more thought

--- src/Blazor.Gitter.Core/Components/Shared/IAppState.cs
+++ src/Blazor.Gitter.Core/Components/Shared/IAppState.cs
@@ -10,6 +10,9 @@ namespace Blazor.Gitter.Core.Components.Shared
         Action GotApiKey { get; set; }
         Action GotChatUser { get; set; }
         Action GotChatRooms { get; set; }
+        event EventHandler ActivityTimeout;
+        event EventHandler ActivityResumed;
+        event EventHandler<DateTime> TimeoutChanged;
         bool HasApiKey { get; }
         bool HasChatRooms { get; }
         bool HasChatUser { get; }
@@ -17,10 +20,13 @@ namespace Blazor.Gitter.Core.Components.Shared
         string GetApiKey();
         List<IChatRoom> GetMyRooms();
         IChatUser GetMyUser();
+        DateTime GetTimeoutTime();
+        string GetLocalTime(DateTime dateTime,string format);
         Task Initialise();
         Task SaveApiKey();
         void SetApiKey(string value);
         void SetMyRooms(List<IChatRoom> value);
         void SetMyUser(IChatUser value);
+        void RecordActivity();
     }
 }
