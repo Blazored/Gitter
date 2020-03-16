e76d965bb3b024c4c261e87b75e42d024080e866
Jan-Willem Spuij <jw@spuij.nl>
2020-03-16 13:52:02 +0100

    Removed BlazorEmbedLibrary

--- src/Blazor.Gitter.Core/Components/App.razor.cs
+++ src/Blazor.Gitter.Core/Components/App.razor.cs
@@ -8,30 +8,5 @@ namespace Blazor.Gitter.Core.Components
 {
     public class AppModel : ComponentBase
     {
-        [Inject] IJSRuntime JSRuntime { get; set; }
-
-        internal List<System.Reflection.Assembly> AssemblyList;
-        private bool HasLoaded;
-        internal bool EmbedRequired => HasLoaded && !( JSRuntime is IJSInProcessRuntime );
-
-        protected override async Task OnInitializedAsync()
-        {
-            await base.OnInitializedAsync();
-
-            AssemblyList = new List<System.Reflection.Assembly>()
-                {
-                    typeof(ILocalStorageService).Assembly,
-                    GetType().Assembly,
-                };
-        }
-        protected override void OnAfterRender(bool FirstRender)
-        {
-            base.OnAfterRender(FirstRender);
-            if (FirstRender)
-            {
-                HasLoaded = true;
-                StateHasChanged();
-            }
-        }
     }
 }
