9fdee6884451081ca289362322954ee63708ddf2
Jan-Willem Spuij <jw@spuij.nl>
2020-03-16 08:32:30 +0100

    css original filename. minified again with correct source map.

--- src/Blazor.Gitter.Core/gulpfile.js
+++ src/Blazor.Gitter.Core/gulpfile.js
@@ -1,18 +1,24 @@
 var gulp = require("gulp"),
     sourcemaps = require("gulp-sourcemaps"),
     sass = require("gulp-sass"),
-    es = require('event-stream');
+    es = require('event-stream'),
+    rename = require("gulp-rename"),
+    cleanCSS = require('gulp-clean-css');
 
 gulp.task("sass", async function () {
     var client = gulp.src('content/css/blazor.gitter.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(rename("blazored.gitter.min.css"))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('../Blazor.Gitter.Client/wwwroot/css'));
 
     var server = gulp.src('content/css/blazor.gitter.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(rename("blazored.gitter.min.css"))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('../Blazor.Gitter.Server/wwwroot/css'));
 
