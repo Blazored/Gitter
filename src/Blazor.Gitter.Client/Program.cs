22c5f7640d005f6df93e0067df037057f7fe755c
Mister Magoo <mister.magoo+githubsql@gmail.com>
2020-06-03 01:42:39 +0100

    Merge pull request #78 from chucker/admin/decouple-Blazor.Gitter.Library

--- src/Blazor.Gitter.Client/Program.cs
+++ src/Blazor.Gitter.Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Blazor.Gitter.Core;
 using Blazor.Gitter.Core.Components;
 using Blazor.Gitter.Core.Components.Shared;
 using Blazor.Gitter.Library;
